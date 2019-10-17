1cb4c845d1c4b8e0b134696010192b9a287bb40b
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2019-10-17 00:50:52 -0500

    add routing and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TypescriptComponent } from './components/typescript/typescript.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+},
+  { path: 'home', component: HomeComponent },
+  { path: 'typescript', component: TypescriptComponent },
+  { path: '**', component: PageNotFoundComponent }
+];  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypescriptComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
