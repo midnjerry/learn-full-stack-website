8c0cdcb19a32e1fdc79119baa49b9b3367932c95
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-06-20 23:42:44 -0500

    Added Object definition

--- src/app/pages/dependency-injection/dependency-injection.component.ts
+++ src/app/pages/dependency-injection/dependency-injection.component.ts
@@ -6,20 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dependency-injection.component.css']
 })
 export class DependencyInjectionComponent implements OnInit {
-  objectSample1 = `public class Car {
-  public void drive(){
-    System.out.println("Car is driving.");
+  objectSample1 = `class Zombie {
+  public void attack(){
+    System.out.println("Zombie attacks!");
   }
 }
   
 public class Program {
   public static void main {
-    Car carInstance = new Car();
-    carInstance.drive();
+    Zombie zombie1 = new Zombie();
+    // Notice that we call the method from zombie1
+    // zombie1 is the object... the working version of the Zombie
+    zombie1.attack();
   }
 }`
-  objectSampleOutput1 = "Car is driving."
+  objectSampleOutput1 = "Zombie attacks!"
   
+  objectSampleError1 = `class Zombie {
+    public void attack(){
+      System.out.println("Zombie attacks!");
+    }
+  }
+    
+  public class Program {
+    public static void main {
+      // This will produce an error!
+      Zombie.attack();
+      // Why?  Because blueprints can't attack!
+    }
+  }`
+    objectSampleErrorOutput1 = "Non-static method 'attack()' cannot be referenced from a static context."
+
+  objectSample2 = `class Zombie {
+    public int hitPoints = 100;
+
+    public void attack(){
+      System.out.println("Zombie attacks!");
+    }
+  }
+    
+  public class Program {
+    public static void main {
+      Zombie z1 = new Zombie();
+      Zombie z2 = new Zombie();
+      Zombie z3 = new Zombie();
+      z2.hitPoints = 50;
+      z3.hitPoints = 75;
+      
+      // Each object has its own state
+      System.out.println("Zombie1 hitpoints: " + z1.hitPoints);
+      System.out.println("Zombie2 hitpoints: " + z2.hitPoints);
+      System.out.println("Zombie3 hitpoints: " + z3.hitPoints);
+    }
+  }`
+    objectSampleOutput2 = `Zombie1 hitpoints: 100
+Zombie2 hitpoints: 50
+Zombie3 hitpoitns: 75`
+    
+
+
 codeExample1 = `public class Person {
   String firstName;
   String lastName;
