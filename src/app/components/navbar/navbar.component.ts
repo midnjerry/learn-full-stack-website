edc9ff2d9776201afb698bbe7d1cabe82e8cf15b
Kianek <kallridge92@gmail.com>
2020-07-23 10:55:06 -0700

    Add base dropdown management

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from "@angular/core";
+import Route from "src/app/models/route";
+import { routes } from "./routes";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
+  routes: Route[];
+  dropdownClass = {
+    show: false,
+  };
+  buttonClass = {
+    active: false,
+  };
 
-  constructor() { }
+  constructor(private _el: ElementRef) {}
 
   ngOnInit() {
+    this.routes = routes;
   }
 
+  toggleMenu() {
+    this.dropdownClass.show = !this.dropdownClass.show;
+    this.buttonClass.active = this.dropdownClass.show ? true : false;
+  }
+
+  @HostListener("window:click", ["$event"])
+  onOutsideClick(event: Event) {
+    event.stopPropagation();
+    if (!this._el.nativeElement.contains(event.target)) {
+      this.dropdownClass.show = false;
+    } else {
+      return;
+    }
+  }
 }
